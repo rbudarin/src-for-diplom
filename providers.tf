8d0413fa43dbabe94663efe995af966eb5261608
Roman <r.bud4rin@ya.ru>
2025-06-18 15:44:01 -0400

    providers.tf

---
+++ providers.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">=1.12.0"
+}
+
+provider "yandex" {
+  cloud_id                 = var.yc_cloud_id
+  folder_id                = var.yc_folder_id
+  zone                     = var.yc_zone
+  service_account_key_file = file("~/.ssh/ya/.authorized_key.json")
+}
